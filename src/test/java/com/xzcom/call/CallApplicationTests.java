f622eaeff6752cf2db82c6b862fea7ad0a7cb6ec
cloudycrane <cloudycrane@gmail.com>
2018-09-10 01:52:31 +0800

    initialization

---
+++ src/test/java/com/xzcom/call/CallApplicationTests.java
@@ -0,0 +1,35 @@
+package com.xzcom.call;
+
+import com.alibaba.fastjson.JSON;
+import com.iflytek.msp.cpdb.lfasr.client.LfasrClientImp;
+import com.iflytek.msp.cpdb.lfasr.exception.LfasrException;
+import com.iflytek.msp.cpdb.lfasr.model.Message;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CallApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        LfasrClientImp lc = null;
+        try {
+            lc = LfasrClientImp.initLfasrClient();
+        } catch (LfasrException e) {
+            // 初始化异常，解析异常描述信息
+            Message initMsg = JSON.parseObject(e.getMessage(), Message.class);
+            System.out.println("ecode=" + initMsg.getErr_no());
+            System.out.println("failed=" + initMsg.getFailed());
+
+            // 用户可根据错误码及错误描述进行相关处理-多为配置参数问题
+        }
+        assertTrue(1==1);
+    }
+
+}
